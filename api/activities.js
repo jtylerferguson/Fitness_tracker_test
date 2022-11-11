58dab16df1429c3025da0e46011d6c79f109f267
timothy <timothybrennan8@gmail.com>
2022-11-11 17:05:11 -0500

    finished activities

--- api/activities.js
+++ api/activities.js
@@ -1,55 +1,56 @@
-const express = require('express');
-const { response } = require('../app');
-const { getAllActivities, createActivity, getActivityById, getActivityByName } = require('../db');
-const { requireUser } = require('./utils');
+const express = require("express");
+const { response } = require("../app");
+const {
+  getAllActivities,
+  createActivity,
+  getActivityById,
+  getActivityByName,
+} = require("../db");
+const { requireUser } = require("./utils");
 const activitiesRouter = express.Router();
 
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
 activitiesRouter.get("/", async (req, res, next) => {
-    try {
-        const activities = await getAllActivities();
+  try {
+    const activities = await getAllActivities();
 
-        if (activities){
-            res.send(
-                activities
-            );
-        } 
-
-
-    } catch ({ name, message}) {
-        next({ name, message})
+    if (activities) {
+      res.send(activities);
     }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
-
-
 
 // POST /api/activities
 activitiesRouter.post("/", requireUser, async (req, res, next) => {
-    const { name, description = "" } = req.body;   
-    const postData = {}
-    const activity = await getActivityByName(name);
-    console.log(activity)
-    try {
+  const { name, description = "" } = req.body;
+  const postData = {};
+  const activity = await getActivityByName(name);
 
-        
-      if (!activity) {
+  try {
+    if (!activity) {
       postData.name = name;
       postData.description = description;
-        
+
       const activities = await createActivity(postData);
 
       if (activities) {
-        res.send( activities );
-      } 
+        res.send(activities);
+      }
     } else {
-        next();
+      next({
+        error: "ACtivity already exists ",
+        message: "An activity with name Push Ups already exists",
+        name: `${postData.name}`,
+      });
     }
-    } catch ({ name, message, error }) {
-      next({ name, message, error });
-    }
-  });
+  } catch ({ name, message, error }) {
+    next({ name, message, error });
+  }
+});
 // PATCH /api/activities/:activityId
 
 module.exports = activitiesRouter;
